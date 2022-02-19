7ede7c0ddbb43ccdb46052b592c2c74ff6aa77b2
Cody Lavene <85128425+codylavene@users.noreply.github.com>
2022-02-19 09:22:30 -0600

    basic nav layout (with placeholder stretch goals)

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -8,17 +8,33 @@ import "./NavBar.css";
 const NavBar = () => {
 	const curr_user = useSelector((state) => state.session.user);
 	return (
-		<nav>
-			<ul>
-				<li>
+		<div className="nav--container">
+			<nav>
+				<div className="nav-left--wrapper">
 					<NavLink to="/" exact={true} activeClassName="active">
 						<div className="logo-b--container">
 							<img src={b} alt="logo" className="logo-b" />
-							{/* <b /> */}
 						</div>
 					</NavLink>
-				</li>
-				<li>
+					<div className="search--container">
+						<label>
+							<i className="fa-solid fa-magnifying-glass"></i>
+							<input
+								type="search"
+								placeholder="Search Basebook"
+							></input>
+						</label>
+					</div>
+				</div>
+				<div className="nav-mid--wrapper">
+					<NavLink to={`/feed`} exact={true} activeClassName="active">
+						{" "}
+						<div className="nav-btn--container">
+							<div className="nav-btn--wrapper">
+								<i className="fa-solid fa-house"></i>
+							</div>
+						</div>
+					</NavLink>
 					<NavLink
 						to={`/users`}
 						exact={true}
@@ -31,15 +47,39 @@ const NavBar = () => {
 							</div>
 						</div>
 					</NavLink>
-				</li>
-				<li>
+				</div>
+				<div className="nav-end--wrapper">
+					<NavLink
+						to={`/users/${curr_user.id}`}
+						exact={true}
+						activeClassName="active--right"
+					>
+						<div className="user-profile-link--container">
+							<div className="image-placeholder nav-user-image"></div>
+							<div className="nav--user-name">
+								{curr_user.first_name}
+							</div>
+						</div>
+					</NavLink>
+
+					<div className="user-menu--grid">
+						<i className="fa-solid fa-ellipsis-vertical"></i>
+						<i className="fa-solid fa-ellipsis-vertical"></i>
+						<i className="fa-solid fa-ellipsis-vertical"></i>
+					</div>
+					<div className="user-menu--grid">
+						<i className="fa-brands fa-facebook-messenger"></i>
+					</div>
+					<div className="user-menu--grid">
+						<i className="fa-solid fa-bell"></i>
+					</div>
 					<div className="user-menu--drop">
 						<i className="fa-solid fa-caret-down"></i>
 					</div>
-					<LogoutButton />
-				</li>
-			</ul>
-		</nav>
+					{/* <LogoutButton /> */}
+				</div>
+			</nav>
+		</div>
 	);
 };
 
