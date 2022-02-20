92c09bdb968020a64531fabc25c79bd5cceed868
Cody Lavene <85128425+codylavene@users.noreply.github.com>
2022-02-20 17:05:01 -0600

    basic footer layout

--- react-app/src/components/Pages/Splash/index.js
+++ react-app/src/components/Pages/Splash/index.js
@@ -3,23 +3,27 @@ import LoginForm from "../../auth/LoginForm";
 // import { ReactComponent as basebook } from "../../../assets/images/basebook.svg";
 import basebook from "../../../assets/images/basebook.png";
 import "./Splash.css";
+import Footer from "./Footer";
 const SplashPage = (props) => {
 	return (
-		<div className="splash-container">
-			<div className="splash-left">
-				<div className="basebook-wrapper">
-					<img src={basebook} alt="text-logo-basebook" required />
-					{/* <basebook /> */}
+		<>
+			<div className="splash-container">
+				<div className="splash-left">
+					<div className="basebook-wrapper">
+						<img src={basebook} alt="text-logo-basebook" required />
+						{/* <basebook /> */}
+					</div>
+					<h2>
+						Connect with friends and the world around you on
+						Basebook.
+					</h2>
 				</div>
-				<h2>
-					Connect with friends and the world around you on Basebook.
-				</h2>
+				<div className="splash-right">
+					<LoginForm />
+				</div>
 			</div>
-			<div className="splash-right">
-				<LoginForm />
-			</div>
-		</div>
-		// FOOTER
+			<Footer />
+		</>
 	);
 };
 
