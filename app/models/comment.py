f84e077cc4003f7435617c56141ed8c19c3157ef
Cody Lavene <85128425+codylavene@users.noreply.github.com>
2022-02-20 15:23:35 -0600

    add uuids to comments, posts and users. add joined_at column to users

--- app/models/comment.py
+++ app/models/comment.py
@@ -1,17 +1,20 @@
 from app.models.user import User
 from .db import db
 from datetime import datetime
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
 
 
 class Comment(db.Model):
     __tablename__ = 'comments'
 
     id = db.Column(db.Integer, primary_key=True)
+    uuid = db.Column(UUID(as_uuid=True), nullable=False, default=uuid.uuid4, unique=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), nullable=False)
     comment_body = db.Column(db.String(280), nullable=False)
-    posted_at = db.Column(db.DateTime, nullable=False, default=datetime.now())
-    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now())
+    posted_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
 
     user = db.relationship("User", back_populates="comments")
     post = db.relationship("Post", back_populates="comments")
