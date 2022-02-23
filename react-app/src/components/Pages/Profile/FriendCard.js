3fc7d66f47a876e3eb0cee01a034ea613793398d
Cody Lavene <85128425+codylavene@users.noreply.github.com>
2022-02-23 11:38:15 -0600

    debug signup validate

--- react-app/src/components/Pages/Profile/FriendCard.js
+++ react-app/src/components/Pages/Profile/FriendCard.js
@@ -4,7 +4,9 @@ import { NavLink } from "react-router-dom";
 const FriendCard = ({ friend }) => {
 	return (
 		<div className="single-friend--card">
-			<div className="image-placeholder--square"></div>
+			<div className="image-placeholder--square">
+				{/* <img src="https://via.placeholder.com/9" alt="default"></img> */}
+			</div>
 			<div className="friend-details">
 				<NavLink to={`/users/${friend?.id}`}>
 					{`${friend?.first_name} ${friend?.last_name}`}
